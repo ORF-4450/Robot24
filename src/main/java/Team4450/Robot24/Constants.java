1904b98b3bbdd22b10590a797e037423e4ad87b1
rac@racc.com <rac@racc.com>
2024-02-07 16:41:20 -0800

    Edits to Adams push.

--- src/main/java/Team4450/Robot24/Constants.java
+++ src/main/java/Team4450/Robot24/Constants.java
@@ -23,7 +23,7 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public final class Constants
 {
-	public static String		PROGRAM_NAME = "ORF24-02.06.24-1";
+	public static String		PROGRAM_NAME = "ORF24-02.07.24-1";
 
 	public static Robot			robot;
 
